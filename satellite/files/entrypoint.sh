26a6efb32bf7204d93f4040f41885e7f3583a016
goldyfruit <gaetan.trellu@gmail.com>
2024-07-11 21:19:31 -0400

    [satellite] Levarage identity file and siteid

--- satellite/files/entrypoint.sh
+++ satellite/files/entrypoint.sh
@@ -3,6 +3,9 @@
 # satellite.list file
 satellite_list=~/.config/mycroft/satellite.list
 
+# _identity.json file
+identity_file=~/.config/hivemind/_identity.json
+
 # Install STT/TTS plugins, microphone plugins or others Python libraries via pip command when a setup.py exists
 if test -f "$satellite_list"; then
     pip3 install -r "$satellite_list"
@@ -18,5 +21,9 @@ elif pw-link --links &> /dev/null; then
     echo -e 'pcm.!default pipewire\nctl.!default pipewire' > ~/.asoundrc
 fi
 
-# Run hivemind-voice-sat
-hivemind-voice-sat --key "$VOICE_SAT_KEY" --password "$VOICE_SAT_PASSWORD" --host "$VOICE_SAT_HOST" --port "$VOICE_SAT_PORT"
+# Check if _identify file exists and leverage it
+if test -f "$identity_file"; then
+    hivemind-voice-sat
+else
+    hivemind-voice-sat --key "$VOICE_SAT_KEY" --password "$VOICE_SAT_PASSWORD" --host "$VOICE_SAT_HOST" --port "$VOICE_SAT_PORT" --siteid "$HIVEMIND_SITEID"
+fi
