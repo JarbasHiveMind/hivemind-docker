435290e9f08e5220445f000b7bf6450051fe9116
goldyfruit <gaetan.trellu@gmail.com>
2024-12-23 11:25:04 -0500

    Modernize, update, improve :)

--- satellite/files/entrypoint.sh
+++ satellite/files/entrypoint.sh
@@ -2,13 +2,17 @@
 
 # satellite.list file
 satellite_list=~/.config/mycroft/satellite.list
+satellite_list_state=~/.local/state/mycroft/satellite.state
 
 # _identity.json file
 identity_file=~/.config/hivemind/_identity.json
 
 # Install STT/TTS plugins, microphone plugins or others Python libraries via pip command when a setup.py exists
-if test -f "$satellite_list"; then
-    pip3 install -r "$satellite_list"
+if [ -n "$satellite_list" ]; then
+    if ! diff -q -B <(grep -vE '^\s*(#|$)' "$satellite_list") <(grep -vE '^\s*(#|$)' "$satellite_list_state" 2>/dev/null) &>/dev/null; then
+        pip3 install --no-cache-dir -r "$satellite_list"
+        cp "$satellite_list" "$satellite_list_state"
+    fi
 fi
 
 # Clear Python cache
@@ -19,10 +23,10 @@ asoundrc_file=~/.asoundrc
 if test -f ~/.config/mycroft/asoundrc; then
     cp -rfp ~/.config/mycroft/asoundrc "$asoundrc_file"
 else
-    if pactl info &>/dev/null; then
-        echo -e 'pcm.!default pulse\nctl.!default pulse' >"$asoundrc_file"
-    elif pw-link --links &>/dev/null; then
+    if pw-link --links &>/dev/null; then
         echo -e 'pcm.!default pipewire\nctl.!default pipewire' >"$asoundrc_file"
+    elif pactl info &>/dev/null; then
+        echo -e 'pcm.!default pulse\nctl.!default pulse' >"$asoundrc_file"
     fi
 fi
 
