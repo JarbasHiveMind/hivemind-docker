8492bcf77af7fae8fa978a5a90af6e853dd3a9ac
goldyfruit <gaetan.trellu@gmail.com>
2025-02-14 08:41:00 -0500

    [listener] Minor fixes

--- listener/files/entrypoint.sh
+++ listener/files/entrypoint.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 # Install plugins via pip command when a setup.py exists
-mkdir -p ~/.config/hivemind ~/.local/state/hivemind
-hivemind_list=~/.config/hivemind/hivemind.list
-hivemind_list_state=~/.local/state/hivemind/hivemind.state
-if test -f "$hivemind_list"; then
-    if ! diff -q -B <(grep -vE '^\s*(#|$)' "$hivemind_list") <(grep -vE '^\s*(#|$)' "$hivemind_list_state" 2>/dev/null) &>/dev/null; then
-        if pip3 install --no-cache-dir -r "$hivemind_list"; then
-            cp "$hivemind_list" "$hivemind_list_state"
+listener_list=~/.config/hivemind/listener.list
+listener_list_state=~/.local/state/hivemind/listener.state
+
+if test -f "$listener_list"; then
+    if ! diff -q -B <(grep -vE '^\s*(#|$)' "$listener_list") <(grep -vE '^\s*(#|$)' "$listener_list_state" 2>/dev/null) &>/dev/null; then
+        if pip3 install --no-cache-dir -r "$listener_list"; then
+            cp "$listener_list" "$listener_list_state"
         else
-            echo "Error: Failed to install packages from $hivemind_list"
+            echo "Error: Failed to install packages from $listener_list"
             exit 1
         fi
     fi
 fi
-
-# Clear Python cache
-rm -rf ~/.cache/pip
 
 # Run hivemind-core
 if ! hivemind-core listen; then
